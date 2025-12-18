6f9de2c9701d38fb3a971c616fa55917d9ca33a4
Ruan Victor <ruanvictorparelhas@gmail.com>
2025-12-18 08:20:18 -0300

    sql na sala

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -2,8 +2,8 @@ import {Pool} from 'pg';
 
 export const pool = new Pool({
     user: 'postgres',
-    password: '123',
+    password: 'pabd',
     host: 'localhost',
     database: 'iflix',
-    port: 5433
+    port: 5432
 });
